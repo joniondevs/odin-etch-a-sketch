576a359ca0f746830aa60fa68d0c47217ecabdc3
jonion <joniondev@gmail.com>
2024-04-19 15:13:21 +0300

    Add logic for drawing grid html structure.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,23 @@
-console.log("==== Etch a Sketch Program ====");
+let gridSize = 16;
+const gridContainer = document.querySelector("#grid-container");
+drawGrid(gridSize, gridSize);
+
+function drawGrid(width, height){
+    if(typeof width != "number" || typeof height != "number"){ return "ERROR: Grid width and height must be numbers!"}
+    
+    for(let y = 0; y < height; y++){
+        let gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        
+        for(let x = 0; x < width; x++)
+        {
+            let cell = document.createElement("div");
+            cell.classList.add("grid-cell");
+            cell.textContent = `${x}-${y}`;
+            gridRow.appendChild(cell);
+        }
+
+        gridContainer.appendChild(gridRow);
+    }
+}
+
